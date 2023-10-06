63e689034253f13ed140575964e20f030c14f346
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-10-06 15:20:25 -0300

    aula-testes-unit-05

--- aula-testes-unit-05/tests/unit/pacientes.test.ts
+++ aula-testes-unit-05/tests/unit/pacientes.test.ts
@@ -9,14 +9,15 @@ describe("Protocol generation test", () => {
     const firstName = faker.person.firstName();
     const lastName = faker.person.lastName();
     const priority = faker.datatype.boolean();
-    const protocol = generateProtocolForPacient(firstName, lastName, priority);
-    expect(protocol).toEqual(
-      {
-        priority,
-        date: expect.any(Date),
-        pacient: `${firstName} ${lastName}`,
-        protocol: "mock protocol"
-      }
-    )
+    const result = generateProtocolForPacient(firstName, lastName, priority);
+    expect(result.protocol).toBe("mock protocol")
+    // expect(protocol).toEqual(
+    //   {
+    //     priority,
+    //     date: expect.any(Date),
+    //     pacient: `${firstName} ${lastName}`,
+    //     protocol: "mock protocol"
+    //   }
+    // )
   });
 });
