d8a169ac556e1968a2df78d80554062dacebed52
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-10-06 20:02:34 -0300

    aula-testes-unit-06

--- aula-testes-unit-06/tests/unit/orders.test.ts
+++ aula-testes-unit-06/tests/unit/orders.test.ts
@@ -4,6 +4,10 @@ import { createOrder, getOrderByProtocol } from "../../src/order-service";
 import * as orderRepository from "../../src/order-repository";
 import { OrderInput } from "../../src/validator";
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe("Order Service Tests", () => {
   it("should create an order", async () => {
     const orderInput: OrderInput = {
@@ -16,23 +20,49 @@ describe("Order Service Tests", () => {
       status: "IN_PREPARATION"
     }
 
-    jest.spyOn(orderRepository, "create").mockImplementation((): any => {
+    jest.spyOn(orderRepository, "create").mockImplementationOnce((): any => {
       return orderOutput
     })
 
-    const order = await createOrder(orderInput);
+    const result = await createOrder(orderInput);
 
     expect(orderRepository.create).toBeCalledTimes(1);
-    expect(order).toEqual(orderOutput);
+    expect(result).toEqual(orderOutput);
   });
 
   it("should return an order based on the protocol", async () => {
-    // TODO
-    expect(true).toBe(true);
+    jest.spyOn(orderRepository, "getByProtocol").mockImplementationOnce((protocol): any => {
+      return {
+        protocol,
+        status: "IN_PREPARATION"
+      }
+    });
+
+    const protocol = "fake protocol";
+
+    const result = await getOrderByProtocol(protocol);
+
+    expect(orderRepository.getByProtocol).toBeCalledTimes(1);
+    expect(result).toEqual(
+      {
+        protocol,
+        status: "IN_PREPARATION"
+      }
+    );
   });
 
   it("should return status INVALID when protocol doesn't exists", async () => {
-    // TODO
-    expect(true).toBe(true);
+    const mock = jest.spyOn(orderRepository, "getByProtocol");
+    mock.mockImplementationOnce((): any => {
+      return undefined;
+    });
+
+    const protocol = "does_not_exists_protocol";
+    const order = await getOrderByProtocol(protocol);
+    expect(orderRepository.getByProtocol).toBeCalledTimes(1);
+    expect(order).toEqual({
+      protocol,
+      status: "INVALID"
+    });
   });
 });
